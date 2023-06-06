f7e239f94f7c7ddc1c3bc06e3c90b975f05ed64a
andresinpa <andresinfantepaez@gmail.com>
2023-06-05 20:51:08 -0500

    Styled-components aplicados correctamenta además de cambio de tema claro-oscuro.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from "react";
-
+import React, {useState} from "react";
+import { temaClaro, temaOscuro } from "./Components/UI/temas";
 import Container from "./Components/Container";
 import Header from "./Components/Header";
+import GlobalStyle from "./GlobalStyles";
+import { ThemeProvider } from "styled-components";
+import { BtnTema } from "./Components/UI";
+import SwitcherTema from "./Components/SwitcherTema";
 
 function App() {
+  const [tema, setTema] = useState(true);
+  const toggleTema = () => {
+    setTema((tema) => !tema)
+  }
+
   return (
-    <>
+    <ThemeProvider theme={ tema ? temaClaro : temaOscuro }>
+
+      <GlobalStyle />
+      <BtnTema onClick={toggleTema}>
+        <SwitcherTema tema={tema}/>
+      </BtnTema>
       <Header />
       <Container />
-    </>
+    </ThemeProvider>
   );
 }
 
